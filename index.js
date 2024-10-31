45499aee5ec82f25517d0fc36733baa491f5849c
kenzo <163335578+moriveira@users.noreply.github.com>
2024-10-31 20:06:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+    const scrollY1 = window.scrollY;
+    const aboutElement1 = document.querySelector('#about');
+
+    
+    if (scrollY1 > 550) {
+        aboutElement1.style.opacity = '100%';
+    } else {
+        aboutElement1.style.opacity = '0%';
+    }
+});
